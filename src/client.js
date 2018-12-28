8b3941aa958a4fa0911dd02f3f1c643c81b14b85
george goldschmied <george.goldschmied@verizon.net>
2018-12-27 20:07:20 -0500

    12/27b

--- src/client.js
+++ src/client.js
@@ -1,13 +1,37 @@
+'use strict';
 
 document.getElementById("demo").innerHTML = myFunction();
 
-function myFunction() {
-    var str = '';
+function myFunction() { // Hoisted (function declaration)
+    var str = '';   // Hoisted
     str += "Paragraph changed5.";
+    str += 'abc'.indexOf('b');
+    str += 'abc'.indexOf('x');
+
+    str += getPersons([2, -5, 137])
+
     return str;
 }
 
 
+function getPerson(id) {
+    if (id < 0) {
+        throw new Error('ID must not be negative: '+id);
+    }
+    return { id: id }; // normally: retrieved from database
+}
 
-//document.getElementById("demo").innerHTML = "Paragraph changed2.";
+function getPersons(ids) {
+    var result = [];
+    ids.forEach(function (id) {
+        try {
+            var person = getPerson(id);
+            result.push(person.toString());
+        } catch (exception) {
+            console.log(exception);
+        }
+    });
+    return result.toString();
+}
+
 
